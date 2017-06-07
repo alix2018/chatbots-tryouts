8927fa30a670ada994d27f59c650e103b00d0ddb
Stephanie Alix <stephanie.alix@dxpo02k1a8bdncv.brandloyaltyint.corp>
2017-06-07 11:44:11 +0200

    quizz works

--- kwiz_test.js
+++ kwiz_test.js
@@ -3,11 +3,10 @@ const Kwiz = require('kwiz')
 
 const quizDefinition = {
   questions: [
-    //{message: 'Hey'},
-    {message: 'What is your name?', answer: {type: 'string', id: 'name'}},
-    //{message: '{{answers.name}}, how old are you?', answer: {type: 'int', id: 'age'}},
-    //{message: '{{answers.name}}, are you sure?', answer: {type: 'truthy', hint: 'Yes or No, {{answers.name}} :)', id: 'sure'}},
-    //{message: 'Buy {{answers.name}}'}
+    {message: 'Hi! What is your name?', answer: {type: 'string', id: 'name'}},
+    {message: 'how old are you?', answer: {type: 'int', id: 'age'}},
+    {message: 'are you sure?', answer: {type: 'truthy', hint: 'Yes or No', id: 'sure'}},
+    {message: 'The quizz is complete'}
   ]
 }
 
@@ -15,25 +14,50 @@ const bot = new Telegraf("371748557:AAENQFq2j9ElnjAhBIkQETukcUj-lIjTbpE")
 bot.use(Telegraf.memorySession())
 
 const quiz = new Kwiz(quizDefinition)
+var answer = new String()
+var question = 0
+var nbQ = 3
 
 
 bot.command('start', (ctx) => {
   quiz.start()
     .then((reply) => {
-      console.log(quiz)
+      //console.log(quiz)
+
       // display the first question
-      console.log('Q:', quiz.quiz.questions[0].message)
-      ctx.reply(quiz.quiz.questions[0].message)
-      //bot.use((ctx) => console.log('ctx:', ctx))
-      return quiz.processMessage('John')
-    })
+      console.log('Q:', quiz.quiz.questions[question].message)
+      ctx.reply(quiz.quiz.questions[question].message)
+
+      // when the user write a message
+      bot.on('message', (ctx) => {
+
+        console.log('message:', [ctx.update.message.text])
+        answer = ctx.update.message.text
+
+
+        quiz.processMessage(answer)
+          .then((reply) => {
+            console.log('State:', quiz.getState())
+            question = question+1
+
+            if (quiz.isCompleted() || (question == nbQ)) {
+              question = 3
+              ctx.reply(quiz.quiz.questions[question].message)
+            }
+
+            else {
+              ctx.reply(quiz.state.answers.name + ', ' + quiz.quiz.questions[question].message)
+            }
+          })
+      })
+
+    .catch((e) => console.log('error:', e))
     /*
     .then((reply) => {
       return quiz.processMessage()
     })
     */
-    .then((reply) => {
-      console.log('State:', quiz.getState())
+
     })
 })
 
