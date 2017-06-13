916ec29a19813527f463fb07d86965fcaa4fcf51
Stephanie Alix <stephanie.alix@dxpo02k1a8bdncv.brandloyaltyint.corp>
2017-06-13 17:34:55 +0200

    node Api.ai changes

--- api_bot/main.js
+++ api_bot/main.js
@@ -4,17 +4,25 @@ const Telegraf = require('../telegraf_bot/node_modules/telegraf')
 const app = apiai("291ec8ecde384312a9c7190faae3761f");
 const bot = new Telegraf("329701042:AAHTHfBD_CHiXStfc8RicqU3TJH4z0tMQDM")
 
+// When the user writes a message
 bot.on('message', (ctx) => {
-  console.log('text:', ctx.update.message.text)
+
+  // Retrieve text content
+  console.log('user text:', ctx.update.message.text)
   var text = ctx.update.message.text
 
+  // Send text content to the Api.ai agent
   var request = app.textRequest(text, {
       sessionId: 'ba658112b8044f2e992d1a21f5945303'
   })
 
+  // Api.ai bot agent answer
   request.on('response', function(response) {
-      console.log('answer:', response.result.fulfillment.speech)
+      console.log('answer:', response)
+      console.log('bot text:', response.result.fulfillment.speech)
       var reply = response.result.fulfillment.speech
+
+      //Answer the user on Telegram
       ctx.reply(reply)
   });
 
